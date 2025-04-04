b56720c11e9887c51066560b61134aa398e4d1fc
jeroenmoons <jeroenmoons@me.com>
2025-04-04 17:35:09 +0200

    Cleaner handling of output image buffer

--- src/output/image.rs
+++ src/output/image.rs
@@ -6,7 +6,7 @@ pub struct Image {
     w: u32,
     h: u32,
     path: String,
-    buffer: RgbImage,
+    buffer: Option<RgbImage>,
 }
 
 impl Image {
@@ -15,26 +15,35 @@ impl Image {
             path,
             w,
             h,
-            buffer: Default::default(),
+            buffer: None,
         }
     }
 }
 
 impl Output for Image {
     fn init(&mut self) -> () {
-        self.buffer = RgbImage::new(self.w, self.h)
+        self.buffer = Some(RgbImage::new(self.w, self.h))
     }
 
     fn put_pixel(&mut self, x: u32, y: u32, pixel: Rgb<u8>) {
-        self.buffer.put_pixel(x, y, pixel);
+        if let Some(buffer) = self.buffer.as_mut() {
+            buffer.put_pixel(x, y, pixel);
+        } else {
+            panic!("Pixel buffer is not set, call init first");
+        }
     }
 
     fn save(&mut self) -> () {
         let path = Path::new(&self.path);
 
-        // Save the image to the file system, image format inferred from extension (.png, .jpg, ...)
-        self.buffer.save(path).expect("Failed to save image");
-
-        println!("HelloWorld output image saved to {}", path.display());
+        match self.buffer {
+            Some(ref mut buffer) => {
+                buffer.save(path).expect("Failed to save image");
+                println!("HelloWorld output image saved to {}", path.display());
+            }
+            None => {
+                println!("Buffer not set, nothing to save");
+            }
+        }
     }
 }
