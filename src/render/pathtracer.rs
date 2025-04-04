354a9c5e6f8912b71f990a199404fd86d83f7b45
jeroenmoons <jeroenmoons@me.com>
2025-04-04 17:58:41 +0200

    Quick qnd dirty progress indicator for PathTracer algorithm

--- src/render/pathtracer.rs
+++ src/render/pathtracer.rs
@@ -2,6 +2,8 @@ use crate::output::Output;
 use crate::render::Renderer;
 use crate::scene::scene::Scene;
 use image::Rgb;
+use std::io;
+use std::io::Write;
 
 pub struct PathTracer {}
 
@@ -17,6 +19,14 @@ impl PathTracer {
 
         (r, g, b)
     }
+
+    fn print_progress(total_pixels: usize, count: usize) {
+        if count % 1000 == 0 || count == total_pixels {
+            let percent = count as f32 / total_pixels as f32 * 100.0;
+            print!("\rRendering: {:>5.1}%", percent);
+            io::stdout().flush().unwrap();
+        }
+    }
 }
 
 impl Renderer for PathTracer {
@@ -28,13 +38,22 @@ impl Renderer for PathTracer {
 
         output.init();
 
+        let total_pixels = (w * h) as usize;
+        let mut count = 0;
+
         for x in 0..w {
             for y in 0..h {
+                count += 1;
+
+                Self::print_progress(total_pixels, count);
+
                 let (r, g, b) = Self::calculate_pixel(w as f32, h as f32, x as f32, y as f32);
 
                 output.put_pixel(x, y, Rgb([r, g, b]));
             }
         }
+
+        println!(" -> Done!");
 
         output.save()
     }
