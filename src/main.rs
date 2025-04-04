4c9f5098b8681bc5f04af47230bab14424f3fcc1
jeroenmoons <jeroenmoons@me.com>
2025-04-04 14:23:06 +0200

    Comments to the main file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod render;
 
+// `crate::` is the starting point for paths pointing to modules in the current crate
 use crate::render::pathtracer::PathTracer;
+// Clap is used to define the cli declaratively
 use clap::{Parser, Subcommand};
 
 // Default output image dimensions
 const DEFAULT_WIDTH: u32 = 800;
 const DEFAULT_HEIGHT: u32 = 600;
 
+// Clap maps cli command string to this struct
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
@@ -14,6 +17,7 @@ struct Cli {
     command: Option<Commands>,
 }
 
+// Clap feature to map subcommand args to an enum variant
 #[derive(Subcommand)]
 enum Commands {
     Render {
