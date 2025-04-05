875151e477dfc1d4a9ec73799920ce8bc907b36b
jeroenmoons <jeroenmoons@me.com>
2025-04-05 12:41:31 +0200

    Basic render time measurement

--- src/output/image.rs
+++ src/output/image.rs
@@ -1,12 +1,14 @@
 use crate::output::Output;
 use image::{Rgb, RgbImage};
 use std::path::Path;
+use std::time::Instant;
 
 pub struct Image {
     w: u32,
     h: u32,
     path: String,
     buffer: Option<RgbImage>,
+    init_instant: Option<Instant>,
 }
 
 impl Image {
@@ -16,13 +18,15 @@ impl Image {
             w,
             h,
             buffer: None,
+            init_instant: None,
         }
     }
 }
 
 impl Output for Image {
     fn init(&mut self) -> () {
-        self.buffer = Some(RgbImage::new(self.w, self.h))
+        self.buffer = Some(RgbImage::new(self.w, self.h));
+        self.init_instant = Some(Instant::now());
     }
 
     fn put_pixel(&mut self, x: u32, y: u32, pixel: Rgb<u8>) {
@@ -39,7 +43,11 @@ impl Output for Image {
         match self.buffer {
             Some(ref mut buffer) => {
                 buffer.save(path).expect("Failed to save image");
-                println!("Output image saved to {}", path.display());
+
+                self.init_instant
+                    .map(|s| println!("Render completed in {:.2?}", s.elapsed()));
+
+                println!("Output image saved at {}", path.display());
             }
             None => {
                 println!("Buffer not set, nothing to save");
