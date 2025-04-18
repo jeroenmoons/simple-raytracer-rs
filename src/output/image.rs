8c9164063be99f919636d479a845f1f33382694b
Jeroen Moons <jeroen.moons@klarrio.com>
2025-04-18 17:37:30 +0200

    Gamma correction

--- src/output/image.rs
+++ src/output/image.rs
@@ -21,6 +21,23 @@ impl Image {
             intensity: Interval::new(0.0, 0.999),
         }
     }
+
+    // Transforms a color component from linear space to gamma space (gamma correction)
+    fn linear_to_gamma(&mut self, linear_component: f32) -> f32 {
+        if linear_component > 0. {
+            return linear_component.sqrt();
+        }
+
+        0.
+    }
+
+    //     inline double linear_to_gamma(double linear_component)
+    // {
+    //     if (linear_component > 0)
+    //         return std::sqrt(linear_component);
+
+    //     return 0;
+    // }
 }
 
 impl Output for Image {
@@ -29,14 +46,18 @@ impl Output for Image {
     }
 
     fn put_pixel(&mut self, x: u32, y: u32, c: &Color) {
+        let r = self.linear_to_gamma(c.x());
+        let g = self.linear_to_gamma(c.y());
+        let b = self.linear_to_gamma(c.z());
+
         if let Some(buffer) = self.buffer.as_mut() {
             buffer.put_pixel(
                 x,
                 y,
                 Rgb([
-                    (self.intensity.clamp(c.x()) * 255.) as u8,
-                    (self.intensity.clamp(c.y()) * 255.) as u8,
-                    (self.intensity.clamp(c.z()) * 255.) as u8,
+                    (self.intensity.clamp(r) * 255.) as u8,
+                    (self.intensity.clamp(g) * 255.) as u8,
+                    (self.intensity.clamp(b) * 255.) as u8,
                 ]),
             );
         } else {
